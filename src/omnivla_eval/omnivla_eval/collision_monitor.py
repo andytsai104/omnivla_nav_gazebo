19ecd84b94c5e5fbbefcc31470ed52e1f6d7d7ba
roy0823 <108273917+roy0823@users.noreply.github.com>
2026-04-19 23:56:51 -0700

    collision_monitor.py

--- src/omnivla_eval/omnivla_eval/collision_monitor.py
+++ src/omnivla_eval/omnivla_eval/collision_monitor.py
@@ -5,6 +5,19 @@ Intended purpose:
 - log collision events during evaluation
 """
 
-# TODO:
-# - subscribe to contact / bumper / sim event topics
-# - count collision events
+from sensor_msgs.msg import LaserScan
+
+class CollisionMonitor:
+    def __init__(self, node):
+        self.node = node
+        self.collision_count = 0
+        self.min_dist = 0.25
+        self.is_active = False
+        self.sub = self.node.create_subscription(LaserScan, '/bcr_bot/scan', self.callback, 10)
+
+    def callback(self, msg):
+        if not self.is_active:
+            return
+        valid_ranges = [r for r in msg.ranges if r > 0.01 and r < 10.0]
+        if valid_ranges and min(valid_ranges) < self.min_dist:
+            self.collision_count += 1
